82da931f2c846d9570d8e92d67c78b94cecded4f
Nathan Alison <nathan.f.alison@gmail.com>
2013-02-09 21:29:38 -0600

    Add more tables

--- schema.sql
+++ schema.sql
@@ -1,10 +1,18 @@
-drop table if exists menu;
-create table menu (
+drop table if exists menu_item;
+create table menu_item (
   id integer primary key autoincrement,
   name string not null,
   date integer not null,
-  meal string not null,
+  name string not null,
   servery_id int not null
+);
+
+drop table if exists reviews;
+create table reviews (
+  menu_item_id int not null,
+  net_id string not null,
+  rating integer not null,
+  (menu_item, net_id) primary key
 );
 
 drop table if exists serveries;
@@ -19,3 +27,10 @@ insert into serveries(name) values ('east');
 insert into serveries(name) values ('west');
 insert into serveries(name) values ('sidrich');
 insert into serveries(name) values ('baker');
+
+drop table if exists admins;
+create table admins(net_id string primary key);
+
+insert into admins values ('nfa1');
+
+
